b3a8f8da9e626363da2a34a06a6d9ced6f23e123
AnvilM <211anvil112@gmail.com>
2023-08-06 17:13:22 +0300

    Link open disable element fix 🐛

--- public/static/js/Funcs.js
+++ public/static/js/Funcs.js
@@ -1,5 +1,5 @@
 $('*').click(function(){
-    if($(this)[0].hasAttribute('href')){
+    if($(this)[0].hasAttribute('href') && !$(this)[0].hasAttribute('disabled')){
         if(!$(this)[0].hasAttribute('hrefType') || $(this).attr('hrefType') === 'this'){
             location.href = $(this).attr('href')
         }
